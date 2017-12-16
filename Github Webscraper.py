527848dadbf54e3f503cb9c926e068aceccbc0fd
Tyler <tylerbro54@yahoo.com>
2017-12-16 14:04:33 -0700

    begun work on code to implement changes to image location in html

--- Github Webscraper.py
+++ Github Webscraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, urlretrieve, URLError
+from os import mkdir
 
 
 
@@ -28,27 +29,38 @@ class GithubReadmeHTML():
             line = str(i)
             if(line is not None):
                 if("<img" in line):
-                    data, linkdata =line.split("<img")
+                    data, linkdata = line.split("<img")
                     linkwork1 = linkdata.split('src=', 1)[-1]
                     linkwork2 = str(linkwork1).split(' ', 1)[-1]
                     link = str(linkwork1).replace(str(linkwork2), "")
                     link = link.replace('"', '')
                     new_link = "https://raw.githubusercontent.com" + link
                     new_link = new_link.replace("/raw/", "/")
-                    print(new_link)
+                    #print(new_link)
                     downloadError = self.downloadImage(new_link)
+                    if(downloadError != 1):
+                        if(link in line):
+                            print("true")
+                        linkdata = linkdata.replace(link, new_link)
+                        print(link)
+                        print(linkdata)
 
     def downloadImage(self, new_link):
         errorState = 0
         try:
             imageName = str(new_link).rsplit("/", 1)[1]
             print(imageName)
-            urlretrieve(new_link, imageName)
+            backspace = "\\"
+            try:
+                pass
+                # urlretrieve(new_link, self.projectName + backspace + imageName)
+            except FileNotFoundError:
+                mkdir(self.projectName)
+                urlretrieve(new_link, backspace + self.projectName + backspace + imageName)
 
         except URLError:
             errorState = 1
 
-        print(errorState)
         return errorState
 
 
