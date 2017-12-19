c71aec986052c8944782e2a85a65a11009dfdfd5
Tyler <tylerbro54@yahoo.com>
2017-12-18 17:40:27 -0700

    created feature for command line agent to create single html docs

---
+++ Github Readme to HTML.py
@@ -0,0 +1,24 @@
+HTML_Document_Builder = __import__("HTML Document Builder")
+
+def getUserRequest():
+    print("what would you like to do:\n1. Turn single readme to HTML Document\n2. Turn multiple readmes to HTML "
+          "Documents\n3. Advance Mode\n4. Help")
+    choice = input("Perform Option Number: ")
+
+    if(choice == "1"):
+        results = getUrlandCommand()
+        HTML_Document_Builder.HTMLDocuments(results[1], results[0])
+
+def getUrlandCommand():
+    results = []
+    print("If you do not want to enter a url then hit Enter to skip!")
+    url = input("What is the url: ")
+    results.append(url)
+    print("If you do not want to enter commands to call then hit Enter to skip!")
+    commands = input("What is the commands you want to run: ")
+    results.append(commands)
+    return results
+
+
+if __name__ == "__main__":
+    getUserRequest()
